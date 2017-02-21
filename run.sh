9515530e76b0a7411d7c89a76ba142fe9131da00
Matteo Fusi <fusiled@gmail.com>
2017-02-21 18:09:03 +0100

    deleted trash authorized keys + fixed run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 KEYS_DIR="keys"
+mkdir -p $KEYS_DIR
 echo "Generating keys on your pc (if they don't exist) in $KEYS_DIR directory..."
 cat /dev/zero | ssh-keygen -q -N "" -f $KEYS_DIR/id_rsa
 echo "--Launching vagrant--"
